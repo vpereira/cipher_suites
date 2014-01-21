422d955a967a34d1ae266f1ced9e84cffa4d685a
Victor Pereira <vpereirabr@gmail.com>
2014-01-21 16:53:25 +0100

    refactored code to class

--- cipher_translate.rb
+++ cipher_translate.rb
@@ -1,6 +1,42 @@
 #!/usr/bin/env ruby
 require 'csv'
 
+class CipherTable
+    def initialize(params = {})
+        @csv = nil
+        @csv_file = "cipherlist_db.csv"
+    end
+
+    def open_table
+        if @csv.nil?
+            CSV.table(@csv_file)
+        else
+            @csv
+        end
+    end
+
+    def get_csv_column(column = :cipher_id)
+        open_table.by_col[column]
+    end
+
+    def get_csv_row
+        open_table.by_row
+    end
+
+    def get_csv_rows(rows = [])
+        rows.collect { |r| get_csv_row[r] }
+    end
+
+    def find_entries(column = :cipher_id, keys_to_search = [])
+        found_keys = keys_to_search.collect do |k|
+            get_csv_column(column).index(k)
+        end
+        get_csv_rows(found_keys.compact)
+    end
+
+end
+
+
 def print_help
     puts "params: gnutls_to_openssl|openssl_to_gnutls"
     puts "i.e: ./get_cipher gnutls_to_openssl"
@@ -14,7 +50,7 @@ def print_gnutls_to_openssl
         name, id_1, id_2, protocol = cipher_line.split
         keys_to_search << "#{id_1}#{id_2}" if id_1 =~ /^0x/
     end
-    find_entries(:cipher_id,keys_to_search).each do |r|
+    CipherTable.new.find_entries(:cipher_id,keys_to_search).each do |r|
         puts "#{r[2]} -> #{r[1]}"
     end
 end
@@ -26,32 +62,9 @@ def print_openssl_to_gnutls
         cipher_id.gsub!(/\s+/,"")
         keys_to_search << cipher_id
     end
-    find_entries(:cipher_id,keys_to_search).each do |r|
+    CipherTable.new.find_entries(:cipher_id,keys_to_search).each do |r|
         puts "#{r[1]} -> #{r[2]}"
     end
-end
-
-def open_table
-    CSV.table('cipherlist_db.csv')
-end
-
-def get_csv_column(column = :cipher_id)
-    open_table.by_col[column]
-end
-
-def get_csv_row
-    open_table.by_row
-end
-
-def get_csv_rows(rows = [])
-    rows.collect { |r| get_csv_row[r] }
-end
-
-def find_entries(column = :cipher_id, keys_to_search = [])
-    found_keys = keys_to_search.collect do |k|
-        get_csv_column(column).index(k)
-    end
-    get_csv_rows(found_keys.compact)
 end
 
 if $0 == __FILE__
