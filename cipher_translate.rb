499e2cc4cf5a58221190195ae11919f0e20cc79a
Victor Pereira <vpereirabr@gmail.com>
2014-01-21 17:25:55 +0100

    refactored print methods

--- cipher_translate.rb
+++ cipher_translate.rb
@@ -40,42 +40,50 @@ end
 def print_help
     puts "params: gnutls_to_openssl|openssl_to_gnutls"
     puts "i.e: ./get_cipher gnutls_to_openssl"
-    exit 1
+    []
 end
 
 
-def print_gnutls_to_openssl
+def translate_gnutls_to_openssl
     keys_to_search = []
     STDIN.each_line do |cipher_line|
         name, id_1, id_2, protocol = cipher_line.split
         keys_to_search << "#{id_1}#{id_2}" if id_1 =~ /^0x/
     end
-    CipherTable.new.find_entries(:cipher_id,keys_to_search).each do |r|
-        puts "#{r[2]} -> #{r[1]}"
-    end
+    keys_to_search
 end
- 
-def print_openssl_to_gnutls
+
+def translate_openssl_to_gnutls
     keys_to_search = [] 
     STDIN.each_line do |cipher_line|
         cipher_id = cipher_line.split('-')[0]
         cipher_id.gsub!(/\s+/,"")
         keys_to_search << cipher_id
     end
-    CipherTable.new.find_entries(:cipher_id,keys_to_search).each do |r|
-        puts "#{r[1]} -> #{r[2]}"
-    end
+    keys_to_search
 end
 
 if $0 == __FILE__
+    mode = 0
     ARGV.each do |a|
-        case a
+        keys_to_search = case a
         when "gnutls_to_openssl"
-            print_gnutls_to_openssl
+            mode = 1
+            translate_gnutls_to_openssl
         when "openssl_to_gnutls"
-            print_openssl_to_gnutls
+            mode = 2
+            translate_openssl_to_gnutls
         else
             print_help
+        end
+        if mode > 0
+            CipherTable.new.find_entries(:cipher_id,keys_to_search).each do |r|
+                if mode == 1 
+                    puts "#{r[2]} -> #{r[1]}"
+                else
+                    puts "#{r[1]} -> #{r[2]}"
+                end
+            end
         end
     end 
 end
